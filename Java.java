95a2208973412274f2375570a12343fdd889bbc9
Stina Qvarnström <stinaqv@gmail.com>
2013-03-14 23:06:40 +0100

    reproduces child

--- Java.java
+++ Java.java
@@ -1,48 +1,62 @@
+    import java.util.Random;
 class Java {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    
-  }
+        
+    }
 }
 
 
 
 class Application {
-  private String goal;
-  private int mutationRate;
-  private int numberOfChildren;
+    private String goal;
+    private int mutationRate;
+    private int numberOfChildren;
 
-  public Application(String goal, int mutationRate, int numberOfChildren) {
-    this.goal = goal;
-    this.mutationRate = mutationRate;
-    this.numberOfChildren = numberOfChildren;
-  }
+    public Application(String goal, int mutationRate, int numberOfChildren) {
+        this.goal = goal;
+        this.mutationRate = mutationRate;
+        this.numberOfChildren = numberOfChildren;
+    }
 
 }
 
 // import java.lang.StringBuilder; 
 class Parent {
 
-  private String lookslike;
+    private String lookslike;
 
-  public Parent(String lookslike) {
-    this.lookslike = lookslike;
-  }
-
-  public String reproduceFavouriteChild(String goal, int mutationRate, int numberOfChildren) {
-    return "";
-  }
-
-
-  private String reproduceChild(String parent, int mutationRate) {
-    // StringBuilder builder = new StringBuilder();
-    for(char character : parent.toCharArray()) {
-      //CharacterIterator ist?
-      
+    public Parent(String lookslike) {
+        this.lookslike = lookslike;
     }
-    return "";
-  }
+
+    public String reproduceFavouriteChild(String goal, int mutationRate, int numberOfChildren) {
+        return "";
+    }
+
+
+    public String reproduceChild(String parent, int mutationRate) {
+        StringBuilder builder = new StringBuilder();
+        for(char character : parent.toCharArray()) {
+            builder.append(mutateCharacter(character, mutationRate));
+            
+        }
+        return builder.toString();
+    }
+
+    private char mutateCharacter(char character, int mutationRate) {
+        Random random = new Random();
+
+        if(mutationRate == random.nextInt(mutationRate)+1) {
+            return (char)(random.nextInt(26) + 'A');
+        } else {
+            return character;
+        }
+
+        
+
+    }
 
 }
 
