5d046a6e7be7e5c3348ca2360b9f78daa39869bc
Erik Kronberg <erik.a.kronberg@gmail.com>
2013-03-29 19:53:48 +0100

    Updated testharness to run 100 times for better accuracy

--- testharness.sh
+++ testharness.sh
@@ -3,12 +3,12 @@
 echo "WEASEL Test Harness"
 echo "Running command: $@"
 
-T=$(time (for i in {1..10}
+T=$(time (for i in {1..100}
 do
 	$@ >/dev/null
 done) 2>&1)
 
 echo "10 iterations took: "
 E=$(echo "$T" | tail -n 3 | head -n 1 | cut -c8- | cut -c-5)
-echo "$E / 10" | bc -l | cut -c-5
+echo "$E / 100" | bc -l | cut -c-5
 echo "Finished."
