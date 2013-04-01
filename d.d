c8559e4ab8264b76bfd71b6ba47b7c487012660b
roobie <bjorn.roberg@gmail.com>
2013-04-01 15:55:21 +0200

    added implementation in D

--- d.d
+++ d.d
@@ -2,45 +2,97 @@ import std.stdio;
 import std.random;
 import std.algorithm;
 
-const string uberGenes = "METHINKS IT IS LIKE A WEASEL";
-const string genePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-const float factor = 0.04;
-const int litterSize = 100;
+version(unittest) {
+  import std.regex;
+}
+
+int main () {
+  Genetics.evolution();
+  return 0;
+}
+
+class Genetics {
+private:
+  const string genePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+  static void evolution(Weasel w, int iters) {
+    writefln("%d\t%d\t%s", iters, w.fitness, w.genes);
+    auto m = w.createOffspring();
+    auto fw = Weasel.findFittest(m);
+    if (fw.fitness == uberGenes.length){
+      writefln("%d\t%d\t%s", iters, fw.fitness, fw.genes);
+      return;
+    }
+    evolution(fw, ++iters);
+  }
+
+public:
+  const string uberGenes = "METHINKS IT IS LIKE A WEASEL";
+  const float factor = 0.04;
+  const int litterSize = 100;
+
+  static char randomGene () {
+    return genePool[uniform(0, genePool.length)];
+  }
+
+  static string randomGenes (int len) {
+    char[] result = new char[len];
+    for (int i = 0; i < len; i++) {
+      result[i] = randomGene();
+    }
+    return cast(string)result;
+  }
+
+  static evolution() {
+    auto w = new Weasel(randomGenes(uberGenes.length));
+    evolution(w, 0);
+  }
+
+  unittest {
+    enum n = 100;
+    enum pg = r"[A-Z]*";
+    auto g = randomGenes(n);
+    assert(!match(g, pg).empty);
+    assert(g.length == n);
+  }
+}
 
 class Weasel {
+private:
   string genes;
   int f = -1;
-
-  this (string genes) {
-    this.genes = genes;
-  }
 
   this (Weasel parent) {
     auto ng = new char[parent.genes.length];
     for(int i = 0; i < parent.genes.length; i++) {
-      if (uniform(0.0f, 1.0f) < factor) ng[i] = randGene();
+      if (uniform(0.0f, 1.0f) < Genetics.factor) ng[i] = Genetics.randomGene();
       else ng[i] = parent.genes[i];
     }
     this.genes = cast(string)ng;
   }
 
-  public int fitness() {
+public:
+  this (string genes) {
+    this.genes = genes;
+  }
+
+  int fitness() {
     if (f != -1) return f;
     f = 0;
     for (int i = 0; i < genes.length; i++)
-      if (genes[i] == uberGenes[i]) f++;
+      if (genes[i] == Genetics.uberGenes[i]) f++;
     return f;
   }
 
-  public Weasel[] createOffspring() {
-    auto children = new Weasel[litterSize];
-    for (int i = 0; i < litterSize; i++) {
+  Weasel[] createOffspring() {
+    auto children = new Weasel[Genetics.litterSize];
+    for (int i = 0; i < Genetics.litterSize; i++) {
       children[i] = new Weasel(this);
     }
     return children;
   }
 
-  public static Weasel findFittest(Weasel[] litter) {
+  static Weasel findFittest(Weasel[] litter) {
     auto fittest = litter[0];
     foreach(w; litter) {
       if (w.fitness > fittest.fitness) {
@@ -49,32 +101,15 @@ class Weasel {
     }
     return fittest;
   }
-}
 
-static char randGene () {
-  return genePool[uniform(0, genePool.length)];
-}
-
-static string randomGenes (int len) {
-  char[] result = new char[len];
-  for (int i = 0; i < len; i++) {
-    result[i] = randGene();
+  unittest {
+    auto g = Genetics.randomGenes(Genetics.uberGenes.length);
+    auto w = new Weasel(g);
+    assert(w.genes == g);
+    assert(w.f == -1);
+    assert(w.fitness != -1);
+    assert(w.f != -1);
+    auto wl = w.createOffspring();
+    assert(wl.length == Genetics.litterSize);
   }
-  return cast(string)result;
-}
-
-void evolution(Weasel w, int iters) {
-  writefln("%d\t%d\t%s", iters, w.fitness, w.genes);
-  auto m = w.createOffspring();
-  auto fw = Weasel.findFittest(m);
-  if (fw.fitness == uberGenes.length){
-    writefln("%d\t%d\t%s", iters, fw.fitness, fw.genes);
-    return;
-  }
-  evolution(fw, ++iters);
-}
-
-int main () {
-  evolution(new Weasel(randomGenes(uberGenes.length)), 0);
-  return 0;
 }
