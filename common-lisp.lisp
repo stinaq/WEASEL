bb7e904d7709709da575968cee8b60cfeabc6da8
Bjorn Roberg <bjorn.roberg@gmail.com>
2014-03-10 22:59:29 +0100

    remove pesky string handling... use only symbols :)

--- common-lisp.lisp
+++ common-lisp.lisp
@@ -2,11 +2,11 @@
 
 (defvar factor 0.04)
 (defvar litter-size 100)
-(defvar uber-weasel (coerce "METHINKS IT IS LIKE A WEASEL" 'list))
-(defvar gene-pool "ABCDEFGHIJKLMNOPQRSTUVWXYZ ")
+(defvar uber-weasel '(M E T H I N K S _ I T _ I S _ L I K E _ A _ W E A S E L))
+(defvar gene-pool '(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z _))
 
 (defun random-gene ()
-  (char gene-pool (random (length gene-pool))))
+  (nth (random (length gene-pool)) gene-pool))
 
 (defun random-genes (len)
   (loop for i to (- len 1) collect (random-gene)))
@@ -29,7 +29,7 @@
 
 (defmethod compare-to-goal ((w weasel))
   (reduce #'+ (mapcar #'(lambda (q w)
-                          (if (char= q w) 1 0))
+                          (if (eql q w) 1 0))
                       (genes w) uber-weasel)))
 
 (defun weasel> (a b) ;descending
@@ -41,15 +41,13 @@
     (copy-seq litter) #'weasel>)))
 
 (defun evolution (weasel count)
-  (let ((gene-string (coerce (genes weasel) 'string)))
-    (format t "~D -> ~a~%"
-            count gene-string)
+  (let ((gene-string (genes weasel)))
+    (print (list count gene-string))
     (if (equal
          (genes weasel) uber-weasel)
-        (list count (coerce uber-weasel 'string))
+        (list count uber-weasel)
         (evolution
          (get-best (create-litter weasel)) (+ 1 count)))))
 
 (print (evolution (make-instance 'weasel) 0))
-
 
